314a41870506d8eb6943e58af08d4b6cb42d70f6
Natallia <ntshmlnchuk@gmail.com>
2026-09-19 17:59:59 -0400

    Leaflet images was updated

--- src/pages/CountryDetail.tsx
+++ src/pages/CountryDetail.tsx
@@ -5,8 +5,19 @@ import type { Country } from '../types/Country';
 import { getAllCountries } from '../services/countryService';
 import { Loader } from '../components/Loader';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
-import 'leaflet/dist/leaflet.css'
+import L from 'leaflet';
+import 'leaflet/dist/leaflet.css';
 
+import markerIcon2x from 'leaflet/dist/images/marker-icon-2x.png';
+import markerIcon from 'leaflet/dist/images/marker-icon.png';
+import markerShadow from 'leaflet/dist/images/marker-shadow.png';
+
+delete (L.Icon.Default.prototype as unknown as { _getIconUrl?: unknown })._getIconUrl;
+    L.Icon.Default.mergeOptions({
+    iconUrl: markerIcon,
+    iconRetinaUrl: markerIcon2x,
+    shadowUrl: markerShadow,
+});
 
 export const CountryDetailPage = () => {
     const { code } = useParams<{ code: string }>();
